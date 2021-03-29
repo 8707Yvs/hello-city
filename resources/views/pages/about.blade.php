2739f7a013f16e8779da819c5baa4db181b1b583
YvsCaleb <yvscaleb2000@gmail.com>
2021-03-28 22:54:22 -0800

    Move static pages to pages folder and remove link from about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -15,7 +15,7 @@
         <p> <a href="/">Revenir à la page d'accueil</a> </p>
 
         <footer>
-          <p>&copy; Copyright {{date("Y")}} &middot; <a href="/about_us">About Us</a> </p>
+          <p>&copy; Copyright {{date("Y")}}</p>
         </footer>
     </body>
 </html>
