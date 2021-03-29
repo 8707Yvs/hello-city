83d8556cf05df2308d115e9f53a72a798571977b
YvsCaleb <yvscaleb2000@gmail.com>
2021-03-28 23:07:40 -0800

    remove dupplication code by using layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,21 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>About | Hello-city</title>
-    </head>
-
-    <body class="antialiased">
-        
+@section('content')
 
         <p>Build viwth &hearts; by Yvs Kilunga.</p>
 
         <p> <a href="/">Revenir à la page d'accueil</a> </p>
 
-        <footer>
-          <p>&copy; Copyright {{date("Y")}}</p>
-        </footer>
-    </body>
-</html>
+        
+@endsection
