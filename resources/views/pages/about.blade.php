6ec09a2da1f3ce9df94f35a94bc746280916ac34
Yvs Kilunga <yvscaleb2000@gmail.com>
2021-04-11 12:50:27 +0200

    add styling with TailwindCSS

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,10 +3,12 @@
 @section('title', 'About-Us | ' . config('app.name'))
 
 @section('content')
-        <img src="{{asset('img/chp.png')}}" alt="Book">
+        <img src="{{asset('img/chp.png')}}" alt="Book" class="my-12 raounded shadow-md h-32">
 
-        <p>Build viwth &hearts; by Yvs Kilunga.</p>
+        <h2 class="mb-3 text-lg text-gray-700">
+          Build with <span class="text-pink-500">&hearts;</span> by Yvs Kilunga.
+        </h2>
 
-        <p> <a href="{{route('home')}}">Revenir à la page d'accueil</a> </p>
+        <p> <a href="{{route('home')}}" class="text-indigo-500 hover:text-indigo-900">Revenir à la page d'accueil</a> </p>
 
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,10 +3,10 @@
 
 @section('content')
 
-        <img src="{{asset('img/book.png')}}" alt="Book">
+        <img src="{{asset('img/book.png')}}" alt="Book" class="mt-12 raounded shadow-md h-32">
 
-        <h1>Hello from Quebec!</h1>
+        <h1 class="my-5 text-3xl sm:text-5xl font-semibold text-indigo-600">Hello from Lubumbashi!</h1>
 
-        <p>It's currently {{ date("H:i A") }}.</p>
+        <p class="text-lg text-gray-800">It's currently {{ date("H:i A") }}.</p>
 
 @endsection
